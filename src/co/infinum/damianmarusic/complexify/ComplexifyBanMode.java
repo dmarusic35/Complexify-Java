726dce0323180a0871573ac0776216ce7b5cd0af
dmarusic35 <dmarusic35@gmail.com>
2015-07-28 16:59:50 +0200

    Initial commit

---
+++ src/co/infinum/damianmarusic/complexify/ComplexifyBanMode.java
@@ -0,0 +1,9 @@
+package co.infinum.damianmarusic.complexify;
+
+/**
+ * Created by Damian Marusic on 28.07.15..
+ */
+public enum ComplexifyBanMode {
+	STRICT,
+	LOOSE
+}
---
+++ src/co/infinum/damianmarusic/complexify/ComplexityListener.java
@@ -0,0 +1,8 @@
+package co.infinum.damianmarusic.complexify;
+
+/**
+ * Created by Damian Marusic on 28.07.15..
+ */
+public interface ComplexityListener {
+	void onSuccess(boolean isValid, double complexity);
+}
